a6179173b7dbcf5d8a7aacbe6c597de4e0984d83
Yevheniy Hordiyenko <johnylviv@gmail.com>
2024-08-09 14:19:46 +0300

    feat: Task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];                                      /* Создание пустого стека */
+  let openBr = bracketsConfig.map(pair => pair[0]);    /* Переменная для открывающихся скобок */
+  let closeBr = bracketsConfig.map(pair => pair[1]);   /* Переменная для закрывающихся скобок */
+
+  for (let char of str) {
+    if (openBr.includes(char)) {
+      if (closeBr.includes(char) && array[array.length - 1] === char) {
+        /* Открывающаяся и закрывающаяся скобки одинаковые || */
+        array.pop();          // Удаляем из массива
+      } else 
+        array.push(char);     // Добавляем в массив
+    } else {
+      // Текущая открывающая скобка = индекс текущей закрывающей скобки
+      let currentOpenBr = openBr[closeBr.indexOf(char)];
+      // Если массив пуст, то нет скобок для сравнения
+      // Либо у нас массив начинается с закрывающей скобки
+      if (array.length === 0 || array.pop() !== currentOpenBr) {
+        return false;
+      }
+    }
+  }
+  return array.length === 0;
 }
